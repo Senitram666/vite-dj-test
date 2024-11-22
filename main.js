17220425932df8e689c1762013fa1165e827f96e
Senitram666 <senitram@gmail.com>
2024-11-22 17:02:42 -0300

    added icons directive

--- main.js
+++ main.js
@@ -23,6 +23,19 @@ Alpine.data('dynamicTable', dynamicTable);
 Alpine.data('dateRangePicker', dateRangePicker);
 Alpine.data('charts', charts);
 
+Alpine.directive('icon', (el, { value, expression }, { evaluate }) => {
+  console.log(value);
+    
+  fetch(`/icons/${expression}.svg`)
+    .then((response) => response.text())
+    .then((html) => {
+      el.innerHTML = html;
+    })
+    .catch((error) => {
+      console.error(`Failed to load icon: ${expression}`, error);
+    });
+});
+
 // Initialize auth store
 document.addEventListener('alpine:init', () => {
   Alpine.store('auth').init();
